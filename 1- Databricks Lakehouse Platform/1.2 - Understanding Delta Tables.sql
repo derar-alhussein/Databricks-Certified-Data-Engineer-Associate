cd3ec26de01df9e72446fb171ec43c998244f71d
derar-alhussein <derar-alhussein>
2023-09-16 01:26:23 +0000

    Insert employees in multiple transactions

--- 1- Databricks Lakehouse Platform/1.2 - Understanding Delta Tables.sql
+++ 1- Databricks Lakehouse Platform/1.2 - Understanding Delta Tables.sql
@@ -4,14 +4,28 @@ CREATE TABLE employees
 
 -- COMMAND ----------
 
+-- NOTE: With latest Databricks Runtimes, inserting few records in single transaction is optimized into single data file.
+-- For this demo, we will insert the records in multiple transactions in order to create 4 data files.
+
 INSERT INTO employees
 VALUES 
   (1, "Adam", 3500.0),
-  (2, "Sarah", 4020.5),
+  (2, "Sarah", 4020.5);
+
+INSERT INTO employees
+VALUES
   (3, "John", 2999.3),
-  (4, "Thomas", 4000.3),
-  (5, "Anna", 2500.0),
+  (4, "Thomas", 4000.3);
+
+INSERT INTO employees
+VALUES
+  (5, "Anna", 2500.0);
+
+INSERT INTO employees
+VALUES
   (6, "Kim", 6200.3)
+
+-- NOTE: When executing multiple SQL statements in the same cell, only the last statement's result will be displayed in the cell output.
 
 -- COMMAND ----------
 
